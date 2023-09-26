6c5b4fc208e003cb6fe3a8129f388a15b789e72e
Md-shefat-masum <mshefat924@gmail.com>
2023-09-26 13:51:37 +0600

    course initialization fixed

--- public/js/frontend_vue.js
+++ public/js/frontend_vue.js
@@ -1,6 +1,5 @@
 
 function initiate_our_course_types() {
-    console.log('our_course_types');
     let our_course_types = document.getElementById('our_course_types');
 
     if (our_course_types) {
