80fc3345e9b24507001ab8c70ca855b89ac215d2
md-shefat-masum <mshefat924@gmail.com>
2024-01-25 15:38:06 +0000

    title update

--- resources/views/backend/course_manager/layout/app.blade.php
+++ resources/views/backend/course_manager/layout/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
-    <title>Home - Vuexy - Bootstrap HTML admin template</title>
+    <title>Course Management</title>
     <link rel="apple-touch-icon" href="{{$meta->fabicon ?? asset(setting(key:'fabicon')) }}">
     {{-- <link rel="shortcut icon" type="image/x-icon" href="{{ asset('backend') }}/app-assets/images/ico/favicon.ico"> --}}
     <link rel="shortcut icon" type="image/x-icon" href="{{$meta->fabicon ?? asset(setting(key:'fabicon')) }}">
